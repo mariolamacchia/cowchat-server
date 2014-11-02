6059e3623ad4b38e791ccd0a68302aeb57343a4b
Mario Lamacchia <mariolamacchia@gmail.com>
2014-11-02 10:38:43 +0100

    Create scripts db and users

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var app = require('express')(),
+    http = require('http').Server(app),
+    io = require('socket.io')(http),
+    db = require('./db'),
+    users = require('./users');
 
 app.get('/', function(req, res){
   res.sendfile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+  
+  socket.on('signup', function(usr) {
+  });
+
+  socket.on('login', function(usr) {
+  });
+
+  socket.on('logout', function(usr) {
+  });
+
   socket.on('message', function(msg){
     io.emit('message', msg);
   });
+
 });
 
 http.listen(3000, function(){
