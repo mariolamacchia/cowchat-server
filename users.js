7d6f7163f405aa0762666d18a54363a3fd2ace1d
Mario Lamacchia <mariolamacchia@gmail.com>
2014-11-02 11:44:00 +0100

    users: implement signup function

--- users.js
+++ users.js
@@ -14,6 +14,20 @@ function comparePassword(pwd1, pwd2, callback) {
 
 module.exports = {
   signup = function(usr, callback) {
+    var password = usr.password;
+    // Check if user exists already
+    db.getUserByUsername(usr.username, function(err, data) {
+      if (err) return callback(err);
+      if (data.username) return callback('Existing user');
+      // Encrypt password
+      encryptPassword(password, function(err, newPassword) {
+        usr.password = newPassword;
+        // Save user in db
+        db.saveUser(usr, function() {
+          return callback();
+        });
+      });
+    });
   },
 
   login = function(username, password, callback) {
