53a58005357ab28e18e65ae8fc0a67e46b53baa9
Mario Lamacchia <mariolamacchia@gmail.com>
2014-11-04 00:52:32 +0100

    Fix typo

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 if (process.env.REDISTOGO_URL) {
     var rtg = require('url').parse(process.env.REDISTOGO_URL),
         redis = require('redis').createClient(rtg.port, rtg.hostname);
-    redis.auth(rtg.auth(split(':')[1]);
+    redis.auth(rtg.auth(split(':')[1]));
 } else {
     var redis = require('redis').createClient();
 }
