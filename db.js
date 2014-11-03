6527af470632b1474e100a9032f57852f96213fe
Mario Lamacchia <mariolamacchia@gmail.com>
2014-11-04 00:51:38 +0100

    Make some changes for deploying

--- db.js
+++ db.js
@@ -1,4 +1,10 @@
-var redis = require('redis').createClient();
+if (process.env.REDISTOGO_URL) {
+    var rtg = require('url').parse(process.env.REDISTOGO_URL),
+        redis = require('redis').createClient(rtg.port, rtg.hostname);
+    redis.auth(rtg.auth(split(':')[1]);
+} else {
+    var redis = require('redis').createClient();
+}
 
 module.exports = {
     saveUser: function(usr, callback) {
