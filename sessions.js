ddb92b8eded120e3e4e5b66edfd3c0a000c0cc72
Mario Lamacchia <mariolamacchia@gmail.com>
2014-11-04 00:23:07 +0100

    Fix error in sessions sendalive

--- sessions.js
+++ sessions.js
@@ -14,28 +14,31 @@ function createSessionId() {
         ("0000" + random).substr(-4);
 }
 
-setInterval(function() {
-    for (var k in users) {
-        console.log('send keepalive to ' + k);
-        var alive = false;
-        users[k].socket.on('alive', function() {
-            console.log(k + ' is alive');
-            alive = true;
-            users[k].socket.removeAllListeners('alive');
-        });
-        users[k].socket.emit('keep alive');
-        setTimeout(function() {
-            if (!alive) {
-                console.log(k + 'is dead');
-                module.exports.delete(k);
-            }
-        }, 5000);
-    }
-}, 5000);
-
 module.exports = {
     create: function(username, socket) {
         users[username] = {session: createSessionId(), socket: socket};
+
+        // Setup keepalive requests
+        var alive = true;
+        socket.on('alive', function() {
+            console.log(username + ' is alive');
+            alive = true;
+        });
+        var keepalive = setInterval(function() {
+            console.log('Senging keepalive to ' + username);
+            alive = false;
+            socket.emit('keep alive');
+            setTimeout(function() {
+                // Do not run if user was disconnected already
+                if (!users[username]) return;
+                if (!alive) {
+                    console.log(username + ' disconnected');
+                    module.exports.delete(username);
+                    clearInterval(keepalive);
+                }
+            }, 10000);
+        }, 5000);
+
         return users[username].session;
     },
     delete: function(username) {
